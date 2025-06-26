22f1131293cf0cecf6c877112a7ad370cd749290
marifercam <fm9705484@gmail.com>
2025-06-26 10:43:18 -0600

    Adding Clase4.java

---
+++ paquete1/Calse4.java
@@ -0,0 +1,21 @@
+package paquete1;
+
+public class Calse4 {
+    private boolean x;
+
+    public boolean isX() {
+        return x;
+    }
+
+    public void setX(boolean x) {
+        this.x = x;
+    }
+
+    @Override
+    public String toString() {
+        return "Calse4 [x=" + x + "]";
+    }
+    
+    
+    
+}
